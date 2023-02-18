9ffe04aba4d87391cfa73e77605f647da48fa857
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2023-02-18 02:11:33 -0300

    Update upload.js

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,18 +1,30 @@
 const multer = require('multer')
+const uuid = require('uuid').v4;
+const path=require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads')
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`)
+        cb(null, uuid() + path.extname(file.originalname).toLocaleLowerCase())
     }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({ 
+    storage: storage,
+    fileFilter:(req,file,cb)=>{
+        const filetypes=/jpeg|jpg|pdf|gif|png/;
+        const mimetype=filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        if(mimetype && extname){
+            return cb(null,true);
+        }
+        cb("Error: Archivo debe ser una pdf o Imagen valida")
+    } })
 
 exports.upload = upload.single('myFile')
 
 exports.uploadFile = (req, res) => {
-    res.send({ data: 'Enviar un archivo' })
+    res.send( req.file )
 }

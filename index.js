064a98de2d09905815eb293428495f240d91a07f
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2023-03-03 19:50:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser=require("body-parser")
 const path=require('path');
+const http = require("http");
+const url = require("url");
 
 require("dotenv").config();
 const userRoutesAPPOINTMENT=require('./src/routes/appointment')
@@ -18,7 +20,7 @@ const userRoutesREGISTER=require('./src/routes/register')
 const userRoutesLABS=require('./src/routes/laboratory')
 const userRoutesSEDE=require('./src/routes/sede')
 const userRoutesDATE=require('./src/routes/date')
-const userRoutesPP=require('./PoliticaPrivacida')
+const rf=require('./PoliticaPrivacida')
 
 //Settings
 const app= express();
@@ -40,14 +42,20 @@ app.use('/api',userRoutesREGISTER)
 app.use('/api',userRoutesLABS)
 app.use('/api',userRoutesSEDE)
 app.use('/api',userRoutesDATE)
-app.use('/api',userRoutesPP)
 app.use(express.static(path.join(__dirname,'uploads')))
 
 //routes
 app.get('/',(req,res)=>{
     res.send(userRoutesPP);
 });
-
+const server = http.createServer((req, res) => {
+    statusCode = 200;
+    contentType =  {'Content-Type': 'text/html'};
+    let t = rf.read_file("text.html");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(t);
+    res.end();
+});
 
 //MongoDB Conection
 mongoose.set('strictQuery', true);

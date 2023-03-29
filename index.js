6e4e163afb6e8d653fc70e111515167141c40b60
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2023-03-29 03:03:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser=require("body-parser")
 const path=require('path');
-//dd
+
 require("dotenv").config();
 const userRoutesAPPOINTMENT=require('./src/routes/appointment')
 const userRoutesPATIENT=require('./src/routes/patient')
